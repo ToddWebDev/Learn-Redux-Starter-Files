18a741319df2593d40c91d38754d602ac3de8892
ToddWebDev <todd.rizzolo@lifeway.com>
2018-06-28 08:49:13 -0500

    Add Reducer Composition

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -12,7 +12,7 @@ const Single = React.createClass({
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props}/>
-        <Comments i={i} postComments={postComments}/>
+        <Comments i={i} postComments={postComments} {...this.props}/>
       </div>
     )
   }
--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -2,8 +2,34 @@
 //1. the action (info about what happened)
 //2. copy of current state
 
+function postComments(state = [], action){
+  switch (action.type) {
+    case 'ADD_COMMENT':
+    return [...state,{
+      user: action.author,
+      text: action.comment
+    }];
+    case 'REMOVE_COMMENT':
+      // return the new state without the deleted comment
+      return [
+        //from the start to the one we want to delete
+        ...state.slice(0, action.index),
+        //after the deleted one, to the end
+        ...state.slice(action.index + 1)
+      ]
+    default: 
+      return state;
+  }
+  return state;
+}
+
 function comments(state = [], action){
-  console.log(state, action);
+  if(typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
